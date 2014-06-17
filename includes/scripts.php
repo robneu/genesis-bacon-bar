d1aa922e2b8610feea495f4d59d7a205862272ca
Robert Neu <rob@auditwp.com>
2014-06-17 04:18:58 -0500

    Started adding style options

--- includes/scripts.php
+++ includes/scripts.php
@@ -21,7 +21,7 @@ defined( 'WPINC' ) or die;
 add_action( 'wp_enqueue_scripts', 'baconbar_enqueue_styles' );
 function baconbar_enqueue_styles() {
 	// Get the theme info.
-	$css_uri = BACON_BAR_URL . 'assets/styles/';
+	$css_uri = BACON_BAR_URL . 'assets/css/';
 
 	// Register the styles.
 	wp_enqueue_style( 'bacon-bar', $css_uri . '/style.css', array(), BACON_BAR_VERSION );
@@ -34,9 +34,19 @@ function baconbar_enqueue_styles() {
  * @since 2.0.0
  */
 function wpbacon_enqueue_scripts() {
-
 	wp_enqueue_script( 'wpbacon-general', wpbacon_get_js_uri() . 'general.js', array( 'jquery' ), '1.0.0', true );
+}
 
+function baconbar_get_style_data() {
+	$field = BACON_SETTINGS_FIELD;
+	$styles = array(
+		'bg_color'           => genesis_get_option( 'baconbar_bg_color', $field ),
+		'text_color'         => genesis_get_option( 'baconbar_text_color', $field ),
+		'button_color'       => genesis_get_option( 'baconbar_button_color', $field ),
+		'button_hover_color' => genesis_get_option( 'baconbar_button_hover_color', $field ),
+		'button_text_color'  => genesis_get_option( 'baconbar_button_text_color', $field ),
+	);
+	return $styles;
 }
 
 /** Load CSS in <head> */
@@ -48,7 +58,7 @@ function wpbacon_enqueue_scripts() {
  */
 function baconbar_meh_css() {
 
-	global $afpw_options;
+	$styles = baconbar_get_style_data();
 
 	$font_size = round( (int) $instance['size'] / 2 );
 	$icon_padding = round ( (int) $font_size / 2 );
@@ -56,13 +66,13 @@ function baconbar_meh_css() {
 	/** The CSS to output */
 	$css = '
 	.awesome-feature .ico-bg a {
-		background: ' . $instance['background_color'] . ' !important;
-		-moz-border-radius: ' . $instance['border_radius'] . 'px
-		-webkit-border-radius: ' . $instance['border_radius'] . 'px;
-		border-radius: ' . $instance['border_radius'] . 'px;
-		color: ' . $instance['icon_color'] . ' !important;
-		font-size: ' . $font_size . 'px;
-		padding: ' . $icon_padding . 'px;
+		background: ' . $styles['background_color'] . ' !important;
+		-moz-border-radius: ' . $styles['border_radius'] . 'px
+		-webkit-border-radius: ' . $styles['border_radius'] . 'px;
+		border-radius: ' . $styles['border_radius'] . 'px;
+		color: ' . $styles['icon_color'] . ' !important;
+		font-size: ' . $styles . 'px;
+		padding: ' . $styles . 'px;
 	}
 
 	.awesome-feature .ico-bg i {
