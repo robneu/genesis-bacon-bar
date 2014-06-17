bdc8452cb50d25dd7014847f4c65fafe8a271ec6
Robert Neu <rob@auditwp.com>
2014-06-17 04:18:43 -0500

    Refactored some functions, added settings

--- includes/functions.php
+++ includes/functions.php
@@ -12,46 +12,120 @@
 // Exit if accessed directly
 defined( 'WPINC' ) or die;
 
-add_action( 'get_header', 'baconbar_setup' );
+// Check an array to see if it has any data.
+function baconbar_array_has_data( $array ) {
+	if ( ! is_array( $array ) || empty( $array ) ) {
+		return false;
+	}
+
+	if ( count( array_filter( $array ) ) !== 0 ) {
+		return true;
+	}
+	return false;
+}
+
+// This function can live wherever is suitable in your plugin
+function baconbar_get_template_part( $slug, $name = null, $load = true ) {
+    global $baconbar_template_loader;
+    $baconbar_template_loader->get_template_part( $slug, $name, $load );
+}
+
+function baconbar_get_data() {
+	$field = BACON_SETTINGS_FIELD;
+	$settings = array(
+		'button_url'   => genesis_get_option( 'baconbar_button_url', $field ),
+		'button_text'  => genesis_get_option( 'baconbar_button_text', $field ),
+		'target_blank' => genesis_get_option( 'baconbar_target_blank', $field ),
+		'teaser_text'  => genesis_get_option( 'baconbar_teaser_text', $field ),
+		'above_site'   => genesis_get_option( 'baconbar_above_site', $field ),
+		'sticky_top'   => genesis_get_option( 'baconbar_sticky_top', $field ),
+		'below_site'   => genesis_get_option( 'baconbar_below_site', $field ),
+		'sticky_foot'  => genesis_get_option( 'baconbar_sticky_foot', $field ),
+	);
+	return $settings;
+}
+
+function baconbar_has_data() {
+	$settings = baconbar_get_data();
+	if ( $settings['button_url'] || $settings['button_text'] || $settings['teaser_text'] ) {
+		return true;
+	}
+	return false;
+}
+
+add_action( 'genesis_meta', 'baconbar_setup' );
 function baconbar_setup() {
+	if ( ! baconbar_has_data() ) {
+		return;
+	}
+	$settings = baconbar_get_data();
 	add_filter( 'body_class', 'baconbar_body_class' );
-	add_action( 'genesis_before', 'baconbar_do_bacon_bar' );
-	//add_action( 'genesis_after', 'baconbar_do_bacon_bar' );
+	if ( $settings['above_site'] ) {
+		add_action( 'genesis_before', 'baconbar_do_header_bar' );
+	}
+	if ( $settings['below_site'] ) {
+		add_action( 'genesis_after', 'baconbar_do_footer_bar' );
+	}
 }
 
 
 //* Add custom body class to the head
 function baconbar_body_class( $classes ) {
+	$settings = baconbar_get_data();
 	$classes[] = 'bacon-bar-active';
+	if ( $settings['sticky_top'] && $settings['above_site'] ) {
+		$classes[] = 'sticky-top';
+	}
+	if ( $settings['sticky_foot'] && $settings['below_site'] ) {
+		$classes[] = 'sticky-bottom';
+	}
 	return $classes;
 }
 
+function baconbar_get_bar_class( $position ) {
+	$settings = baconbar_get_data();
+	$classes  = 'header-bar';
+	if ( $settings['sticky_top'] ) {
+		$classes .= ' fixed-bar';
+	}
+	if ( 'footer' !== $position ) {
+		return $classes;
+	}
+	$classes  = 'footer-bar';
+	if ( $settings['sticky_foot'] ) {
+		$classes .= ' fixed-footer';
+	}
+	return $classes;
+}
+
+add_action( 'baconbar_header_content', 'baconbar_do_content' );
+add_action( 'baconbar_footer_content', 'baconbar_do_content' );
+function baconbar_do_content() {
+	$settings = baconbar_get_data();
+	$target_blank = ! empty( $settings['target_blank'] ) ? 'target="_blank"' : '';
+
+	if ( $settings['teaser_text'] ) {
+		echo '<p class="bacon-text">' . esc_attr( $settings['teaser_text'] ) . '</p>';
+	}
+	if ( $settings['button_url'] && $settings['button_text'] ) {
+		echo '<a class="bacon-button" ' . $target_blank . ' href="' . esc_url( $settings['button_url'] ) . '">' . esc_attr( $settings['button_text'] ) . '</a>';
+	}
+}
+
 /**
- * baconbar_social_sharing_buttons function.
+ * Display a template for the Bacon Bar.
  *
- * This function displays social sharing buttons based on the options selected by the user.
- *
- * @since 1.0.0
- *
+ * @since  4.0.0
  */
-function baconbar_do_bacon_bar() {
-	$field        = BACON_SETTINGS_FIELD;
-	$button_url   = genesis_get_option( 'baconbar_button_url', $field );
-	$button_text  =	genesis_get_option( 'baconbar_button_text', $field );
-	$target_blank =	genesis_get_option( 'baconbar_target_blank', $field );
-	$teaser_text  = genesis_get_option( 'baconbar_teaser_text', $field );
-	$classes = '';
-	$classes .= 'fixed-bar';
+function baconbar_do_header_bar() {
+	baconbar_get_template_part( 'bacon-bar' );
+}
 
-	echo '<div class="bacon-bar ' . $classes . '">';
-		echo '<div class="wrap">';
-
-			if ( $teaser_text ) {
-				echo '<p class="bacon-text">' . esc_attr( $teaser_text ) . '</p>';
-			}
-			if ( $button_url && $button_text ) {
-				echo '<a class="bacon-button" href="' . esc_url( $button_url ) . '">' . esc_attr( $button_text ) . '</a>';
-			}
-		echo '</div>';
-	echo '</div>';
+/**
+ * Display a template for the Bacon Bar.
+ *
+ * @since  4.0.0
+ */
+function baconbar_do_footer_bar() {
+	baconbar_get_template_part( 'bacon-bar', 'footer' );
 }
